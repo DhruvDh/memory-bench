6141f059437693b47516b40dd2f3e302e89fec0a
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2019-09-24 22:30:09 -0400

    please work

--- bandwidth/src/main.rs
+++ bandwidth/src/main.rs
@@ -8,7 +8,7 @@ use std::thread;
 
 const NUM_LOOPS: usize = 100_000;
 const NUM_THREADS: usize = 8;
-const NUM_OF_256BIT_VECTORS: usize = 1023 * 8;
+const NUM_OF_256BIT_VECTORS: usize = 512;
 
 #[inline(always)]
 fn do_read_write (len: usize) -> f32 {
@@ -30,22 +30,24 @@ fn do_read_write (len: usize) -> f32 {
     for _ in 0..NUM_LOOPS {
         for nums in random_ints.iter_mut() {
             *nums = nums.rotate_left(rotate_by);
-            // *nums = std::mem::take(nums);
         }
     }
 
     let time_taken = now.elapsed().as_secs_f32();
-    // dbg!(&random_ints);
+    dbg!(&random_ints);
 
     time_taken
 }
 
 fn main() {
-    let time_taken: f32 = (1..21).map(|i| {
-        let mut threads = vec![];      
+    if std::env::args().len() > 1 {
+        let i: i32 = std::env::args().nth(1).unwrap().parse::<i32>().unwrap();
+        let mut threads = vec![];
+        let multiplier = 2f32.powi(i);
+        println!("Size is {:?} kB.", (NUM_OF_256BIT_VECTORS as f32 * multiplier) as usize * 32/ 1024);
 
         for _ in 0..NUM_THREADS {
-            let size = NUM_OF_256BIT_VECTORS * i;
+            let size = (NUM_OF_256BIT_VECTORS as f32 * multiplier) as usize;
             threads.push(thread::spawn(move || do_read_write(size.clone())))
         }
 
@@ -53,16 +55,41 @@ fn main() {
                                 .map(|t| t.join().unwrap())
                                 .sum();
         
-        println!("Size is {:?} kB.", (NUM_OF_256BIT_VECTORS * i * 32 / 1024));
         println!("{:?} seconds.", time_taken/(NUM_THREADS as f32));
         
-        let memory_used = NUM_OF_256BIT_VECTORS * 32;
-        let bandwidth = ((memory_used * NUM_LOOPS * NUM_THREADS) as f32)/time_taken;
-        let bandwidth = bandwidth / (1024 * 1024 * 1024) as f32;
-        println!("Bandwidth is {:?} GBps.", bandwidth);
-        println!("/n/n");
-        time_taken/(NUM_THREADS as f32)
-    }).sum();
+        let memory_used = (NUM_OF_256BIT_VECTORS as f32 * multiplier) as usize * 32;
+        let bandwidth = ((memory_used * NUM_LOOPS * NUM_THREADS)  as f32)/time_taken;
+        println!("Bandwidth is {:?} GBps.", bandwidth / (10e9));
+    }
+    else {
+        let time_taken: f32 = (0..5).map(|i| {
+            let mut threads = vec![];
+            let multiplier = 2f32.powi(i);
 
-    dbg!(&time_taken);
+            println!("Size is {:?} kB.", (NUM_OF_256BIT_VECTORS as f32 * multiplier) as usize * 32/ 1024);
+
+            for _ in 0..NUM_THREADS {
+                let size = (NUM_OF_256BIT_VECTORS as f32 * multiplier) as usize;
+                threads.push(thread::spawn(move || do_read_write(size.clone())))
+            }
+
+            let time_taken: f32 = threads.into_iter()
+                                    .map(|t| t.join().unwrap())
+                                    .sum();
+            
+            println!("{:?} seconds.", time_taken/(NUM_THREADS as f32));
+
+            dbg!(&multiplier);            
+            dbg!((NUM_OF_256BIT_VECTORS as f32 * multiplier) as usize * 32);
+            let memory_used = (NUM_OF_256BIT_VECTORS as f32 * multiplier) as usize * 32;
+            dbg!(((memory_used * NUM_LOOPS * NUM_THREADS)  as f32)/time_taken);
+            let bandwidth = ((memory_used * NUM_LOOPS * NUM_THREADS)  as f32)/time_taken;
+
+            println!("Bandwidth is {:?} GBps.", bandwidth / (10e9));
+            println!("\n\n");
+            time_taken/(NUM_THREADS as f32)
+        }).sum();
+
+        dbg!(&time_taken);
+    }
 }
